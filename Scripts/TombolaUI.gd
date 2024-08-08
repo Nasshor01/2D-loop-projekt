b1bf759eba6398fbbea59a1a2937bdd74811ba21
Nasshor01 <141824772+Nasshor01@users.noreply.github.com>
2024-08-08 20:06:16 +0200

    Update TombolaUI.gd

--- Scripts/TombolaUI.gd
+++ Scripts/TombolaUI.gd
@@ -5,20 +5,32 @@ extends Control
 @onready var result_label = $Label
 
 @export var min_number = 1
-@export var max_number = 6
+@export var max_number = 24
 
 signal number_drawn(number)
 
 func _ready():
 	result_label.visible = false
+	draw_button.disabled = true # Disable the button initially
 	draw_button.connect("pressed", Callable(self, "_on_draw_button_pressed"))
-	tombola.connect("animation_finished", Callable(self, "_on_tombola_animation_finished"))
+
+func enable_button():
+	draw_button.disabled = false
+
+func disable_button():
+	draw_button.disabled = true
 
 func _on_draw_button_pressed():
-	tombola.play("default")  # Přehrání animace
-	result_label.visible = false  # Skryje text během animace
+	disable_button() # Disable the button immediately after pressing
+	tombola.play("default") # Play the animation
+	result_label.visible = false # Hide the text during animation
 
-func _on_tombola_animation_finished():
+	var frame_count = tombola.sprite_frames.get_frame_count("default")
+	var fps = tombola.sprite_frames.get_animation_speed("default")
+	var animation_duration = frame_count / fps
+
+	await get_tree().create_timer(animation_duration).timeout
+	
 	var drawn_number = randi() % (max_number - min_number + 1) + min_number
 	result_label.text = str(drawn_number)
 	result_label.visible = true
