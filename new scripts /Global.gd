64145b9598b065846b2fb91d64ae5542841c31dc
Nasshor01 <141824772+Nasshor01@users.noreply.github.com>
2024-10-04 12:52:10 +0200

    Create Global.gd

---
+++ new scripts /Global.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var player_data = {}  # Ukládá informace o stavu hráče
+var enemy_data = {}   # Ukládá informace o stavu nepřátel
+var saved_map_state = null  # Uložený stav mapy, použít při návratu z bitevní scény
