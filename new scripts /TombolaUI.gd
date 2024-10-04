be479274fdcdad9cf82b398981cf1ea9c438678b
Nasshor01 <141824772+Nasshor01@users.noreply.github.com>
2024-10-04 12:56:54 +0200

    Create TombolaUI.gd

---
+++ new scripts /TombolaUI.gd
@@ -0,0 +1,38 @@
+extends Control
+
+@onready var tombola = $AnimatedSprite2D
+@onready var draw_button = $Button
+@onready var result_label = $Label
+
+@export var min_number = 1
+@export var max_number = 24
+
+signal number_drawn(number)
+
+func _ready():
+    result_label.visible = false
+    draw_button.disabled = true
+    draw_button.connect("pressed", Callable(self, "_on_draw_button_pressed"))
+
+func enable_button():
+    draw_button.disabled = false
+
+func disable_button():
+    draw_button.disabled = true
+
+func _on_draw_button_pressed():
+    disable_button()
+    tombola.play("default")
+    result_label.visible = false
+
+    var frame_count = tombola.sprite_frames.get_frame_count("default")
+    var fps = tombola.sprite_frames.get_animation_speed("default")
+    var animation_duration = frame_count / fps
+
+    await get_tree().create_timer(animation_duration).timeout
+
+    var drawn_number = randi() % (max_number - min_number + 1) + min_number
+    result_label.text = str(drawn_number)
+    result_label.visible = true
+    emit_signal("number_drawn", drawn_number)
+    enable_button()
